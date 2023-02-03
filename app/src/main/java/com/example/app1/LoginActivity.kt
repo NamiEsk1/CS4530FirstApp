614bca8d7ccca696704784f22ca8f7af2f1cbb14
Nami Eskandarian <u1117098@utah.edu>
2023-02-03 15:49:47 -0700

    Finished assignment
    - Login screen takes names from previous activity
    - States are saved for various cases

--- app/src/main/java/com/example/app1/LoginActivity.kt
+++ app/src/main/java/com/example/app1/LoginActivity.kt
@@ -2,10 +2,32 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    // Key constants
+    companion object {
+        private const val FIRST_NAME_KEY = "First Name"
+        private const val LAST_NAME_KEY = "Last Name"
+    }
+
+    // Variables for user's name and UI text view
+    private var mFullName: String? = null
+    private var mTvLogin: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Get intent and text view
+        val receivedIntent = intent
+        mTvLogin = findViewById(R.id.tv_login)
+
+        // Get the name submitted and show that the user logged in
+        mFullName = receivedIntent.getStringExtra(FIRST_NAME_KEY).plus(" ")
+            .plus(receivedIntent.getStringExtra(LAST_NAME_KEY))
+
+        if (mTvLogin != null && !mFullName.isNullOrBlank())
+            mTvLogin!!.text = mFullName.plus(getString(R.string.loginMessage))
     }
 }
